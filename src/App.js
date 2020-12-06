b3e170fb8f2ccd308a9e4c54eaf33f54982a9c3f
Ayush Mainali <ayushmainali11@gmail.com>
2020-12-06 11:06:26 +0545

    Added Formik, material ui, yup as dependencies

--- src/App.js
+++ src/App.js
@@ -1,11 +1,80 @@
-import './App.css';
+import "./App.css";
+import { Card, CardContent, Typography } from "@material-ui/core";
+import { Formik, Field, Form, FormikConfig, FormikValues } from "formik";
+import { TextField, CheckboxWithLabel } from "formik-material-ui";
+import { object, mixed, number } from "yup";
+
+
+export function FormikStepper({ children, ...props }) {
+  console.log(props)
+  return (
+    <Formik {...props}>
+      <Form autoComplete="off">{children}</Form>
+    </Formik>
+  );
+}
+
 
 function App() {
   return (
-    <div className="App">
-      Hello From Multi step form 🚀🚀
-    </div>
+    <Card>
+      <CardContent>
+        <FormikStepper
+          validationSchema={object({
+            money: mixed().when("millioniare", {
+              is: true,
+              then: number()
+                .required()
+                .min(1_000_000, "Amount most be greater than 1 million USD"),
+              otherwise: number().required(),
+            }),
+          })}
+          initialValues={{
+            firstName: "",
+            lastName: "",
+            millioniare: false,
+            money: 0,
+            description: "",
+          }}
+        >
+
+            <div>
+              <Field
+                name="firstName"
+                component={TextField}
+                label="First Name"
+              />
+              <Field name="lastName" component={TextField} label="Last Name" />
+            </div>
+            <div>
+              <Field
+                name="millioniare"
+                type="checkbox"
+                component={CheckboxWithLabel}
+                Label={{ label: "I am a millioniare" }}
+              />
+              <Field
+                name="money"
+                component={TextField}
+                label="Money I can flex!"
+                type="number"
+              />
+            </div>
+            <div>
+              <Field
+                name="description"
+                component={TextField}
+                label="Description"
+              />
+            </div>
+
+        </FormikStepper>
+      </CardContent>
+    </Card>
   );
 }
 
 export default App;
+
+
+
