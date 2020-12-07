b0c2352abf10faf55428800423abfe2e53f8b376
Ayush Mainali <ayushmainali11@gmail.com>
2020-12-07 07:52:58 +0545

    Updated

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import "./App.css";
-import { Card, CardContent, Typography } from "@material-ui/core";
+import React, { useState, useEffect } from "react";
+import { Card, CardContent, Typography, Button } from "@material-ui/core";
 import { Formik, Field, Form, FormikConfig, FormikValues } from "formik";
 import { TextField, CheckboxWithLabel } from "formik-material-ui";
 import { object, mixed, number } from "yup";
-
-
-export function FormikStepper({ children, ...props }) {
-  console.log(props)
-  return (
-    <Formik {...props}>
-      <Form autoComplete="off">{children}</Form>
-    </Formik>
-  );
-}
-
 
 function App() {
   return (
     <Card>
       <CardContent>
         <FormikStepper
-          validationSchema={object({
+          initialValues={{
+            firstName: "",
+            lastName: "",
+            millioniare: false,
+            money: 0,
+            description: "",
+          }}
+          validateSchema={object({
             money: mixed().when("millioniare", {
               is: true,
               then: number()
@@ -29,52 +26,72 @@ function App() {
               otherwise: number().required(),
             }),
           })}
-          initialValues={{
-            firstName: "",
-            lastName: "",
-            millioniare: false,
-            money: 0,
-            description: "",
-          }}
+          // onSubmit={() => {}}
         >
+          <div>
+            <Field name="firstName" component={TextField} label="First Name" />
+            <Field name="lastName" component={TextField} label="Last Name" />
+            <Field
+              name="millioniare"
+              type="checkbox"
+              component={CheckboxWithLabel}
+              Label={{ label: "I am a millioniare" }}
+            />
+          </div>
 
-            <div>
-              <Field
-                name="firstName"
-                component={TextField}
-                label="First Name"
-              />
-              <Field name="lastName" component={TextField} label="Last Name" />
-            </div>
-            <div>
-              <Field
-                name="millioniare"
-                type="checkbox"
-                component={CheckboxWithLabel}
-                Label={{ label: "I am a millioniare" }}
-              />
-              <Field
-                name="money"
-                component={TextField}
-                label="Money I can flex!"
-                type="number"
-              />
-            </div>
-            <div>
-              <Field
-                name="description"
-                component={TextField}
-                label="Description"
-              />
-            </div>
-
+          <div>
+            <Field
+              name="money"
+              component={TextField}
+              label="Money I can flex!"
+              type="number"
+            />
+          </div>
+          <div>
+            <Field
+              name="description"
+              component={TextField}
+              label="Description"
+            />
+          </div>
         </FormikStepper>
       </CardContent>
     </Card>
   );
 }
 
+// export function FormikStep({ children, ...props }) {
+//   return <div>{children}</div>;
+// }
+
+export function FormikStepper({ children, ...props }) {
+  const childrenArray = React.Children.toArray(children);
+  const [step, setStep] = useState(0);
+  let currentChild = childrenArray[step];
+  const isLastStep = () => {
+    return step === childrenArray.length - 1;
+  };
+  return (
+    <Formik
+      {...props}
+      
+      onSubmit={async (values, helpers) => {
+        if (isLastStep()) {
+          console.log("Hi")
+        } else {
+          setStep((s) => s + 1);
+        }
+      }}
+    >
+      <Form autoComplete="off">
+        {currentChild}
+        {step > 0 ? (
+          <Button onClick={() => setStep((s) => s - 1)}>Back</Button>
+        ) : null}
+        <Button type="submit">{isLastStep() ? "Submit" : "Next"}</Button>
+      </Form>
+    </Formik>
+  );
+}
+
 export default App;
-
-
-
